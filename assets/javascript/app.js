d73a80811b266257a7635a9744d174fb417352bd
Leif Swenson <leifswenson@er-10-132-91-82.wireless.umn.edu>
2018-01-13 09:52:23 -0600

    add firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,16 +2,23 @@
 
 
 // Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC-AM58u5iUbGJuIhN3-GFFXsqf0RQfEUE",
+    authDomain: "remembeer-e06cd.firebaseapp.com",
+    databaseURL: "https://remembeer-e06cd.firebaseio.com",
+    projectId: "remembeer-e06cd",
+    storageBucket: "remembeer-e06cd.appspot.com",
+    messagingSenderId: "952581043095"
+  };
 
+  firebase.initializeApp(config);
 
 
 
 // Variables
 //==================================================================================================
 
-
-
-
+var database = firebase.database();
 
 
 
